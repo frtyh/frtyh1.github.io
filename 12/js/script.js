02d24d1b7f22c506249746fa01a50301efa7cb52
frtyh <65789749+frtyh@users.noreply.github.com>
2020-06-08 15:36:04 +0400

    qwerty13

---
+++ 12/js/script.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+     $('.slider').slick({
+         autoplay: true,
+         autoplaySpeed:2200,
+         speed:3700,
+         arrows: false,
+     
+         
+     });
+ });   
